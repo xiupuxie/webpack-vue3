610dec86bc1ca44b134f0e662cd1eb4a2f36da1f
xxp <2691451749@qq.com>
2024-08-01 22:18:55 +0800

    feat: 生产环境优化

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,43 +1,61 @@
+process.env._MODE = 'production';
+
 const path = require('path');
 const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.config.js');
-const TerserPlugin = require('terser-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-
-module.exports = merge(baseConfig, {
-    module: {
-        rules: [
-            {
-                test: /\.css|scss|sass$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
-        ]
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'public', 'favicon.ico'),
-                    to: path.resolve(__dirname, 'dist/image/')
-                }
-            ]
-        })
-    ],
-    optimization: {
-        usedExports: true,
-        minimize: true,
-        minimizer: [
-            new TerserPlugin(),
-            new MiniCssExtractPlugin({
-                filename: 'index-[contenthash:8].css',
-                chunkFilename: '[id].css'
-            })
-        ]
-    },
-    cache: {
-        type: 'filesystem',
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const baseConfig = require('./webpack.base');
+const config = merge(baseConfig, {
+    output: {
+        path: path.resolve('dist'),
+        filename: 'js/[name].[chunkhash:8].js',
+        chunkFilename: 'js/[name].[chunkhash:8].js'
     },
     mode: 'production',
-    devtool: 'cheap-module-source-map'
-})
+    devtool: 'source-map',
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    name: `chunk-vendors`,
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    chunks: 'initial'
+                },
+                vue: {
+                    name: `chunk-vue`,
+                    test: /[\\/]node_modules[\\/]vue[\\/]/,
+                    priority: 1,
+                    chunks: 'initial'
+                },
+                common: {
+                    name: `chunk-common`,
+                    minChunks: 2,
+                    priority: -20,
+                    chunks: 'initial',
+                    reuseExistingChunk: true
+                }
+            }
+        },
+        runtimeChunk: {
+            name: entrypoint => `runtime-${entrypoint.name}`
+        },
+        minimizer: [
+            // 可以使用 `...` 语法扩展现有的 minimizer
+            new CssMinimizerPlugin(),
+            // 如果需要添加 terser-webpack-plugin，在这里加入它
+            // new TerserPlugin(),
+        ],
+    },
+    plugins: [
+        // new CleanWebpackPlugin(), // 如果需要清除文件可以在这里加入
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+    ],
+});
+``
+
+module.exports = config;
